084c4cb0243f9650da128c309bf46478527c0e9b
aarshvb <aarshvb@gmail.com>
2020-12-17 17:35:29 +0530

    s3 upload amplify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Storage } from 'aws-amplify'
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports' ;
+import {AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+import config from './aws-exports';
+Amplify.configure(config);
+Amplify.configure(awsconfig)
+
+class App extends Component {
+  state = { fileUrl: '', file: '', filename: '' }
+  handleChange = e => {
+    const file = e.target.files[0]
+    this.setState({
+      fileUrl: URL.createObjectURL(file),
+      file,
+      filename: file.name
+    })
+
+  }
+  saveFile = async () => {
+    await Storage.put(this.state.filename, this.state.file)
+      .then(() => {
+        console.log('successful')
+        this.setState({ fileUrl: '', file: '', filename: ''})
+      })
+      .catch(err => {
+        console.log('error', err)
+      })
+  }
+  
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        <h1 className="App-Title"> Welcome to React </h1>
+         
+      
       </header>
+      <input type='file' onChange={this.handleChange} />
+      <img src={this.state.fileUrl} />
+      <button onClick={this.saveFile}>Save File</button>
     </div>
   );
 }
-
-export default App;
+}
+export default withAuthenticator(App, { includeGreetings: true });
